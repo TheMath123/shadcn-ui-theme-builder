680dcc468357d5e80a4782bd6e961955e685ccd4
Matheus P Agostinho <contact@matheuspa.com>
2024-02-15 22:02:10 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,21 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+// Next
+import { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { fontSans } from "./font";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
 
 export const metadata: Metadata = {
-  title: "Creator Theme Shadcn",
-  description: "Theme Creator for Shadcn-ui",
+  title: "Shadcn/UI Theme Builder",
+  description: "Painel de movimentações segurados",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
